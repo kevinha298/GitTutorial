e8830e6deea0d1c1d44a477897eb60b6efe59329
kevinha298 <32086539+kevinha298@users.noreply.github.com>
2019-05-15 18:57:46 -0700

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,13 +2,13 @@ def add(x,y):
     pass
 
 def subtract(x,y):
-    return x-y
+    pass
 
 def multiply(x,y):
-    return x*y
+    pass
 
 def divide(x,y):
-    return x/y
+    pass
 
 def square(x,y):
     pass
